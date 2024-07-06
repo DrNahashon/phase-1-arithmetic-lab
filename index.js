00c6c9b6bffb492b97feef0293f533ba22931a46
DrNahashonMwirigi <mbunahs@gmail.com>
2024-07-06 17:05:24 +0300

    complete lab 4

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+let num3 = 14;
+let num4 = 10;
+
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 100) + 1;
+
+let mod = num3 % num4;
+
+let max = Math.max(5, 10, 20);
+
+console.log("multiply:", multiply);
+console.log("random:", random);
+console.log("mod:", mod);
+console.log("max:", max);
